e539ef4a543b3f775bf14f1fcbbd8f3a04f1c38a
孙江梦 <sunjiangmeng@wondersoft.cn>
2023-09-28 17:55:02 +0800

    tcp兼容

--- server/protocol/tcp/tcp.go
+++ server/protocol/tcp/tcp.go
@@ -1,7 +1,10 @@
 package tcp
 
 import (
+	"encoding/binary"
 	"github.com/panjf2000/gnet/v2"
+	"github.com/panjf2000/gnet/v2/pkg/logging"
+	"go-ygosrv/core/duel"
 	"log"
 )
 
@@ -20,7 +23,34 @@ func (es *EchoServer) OnBoot(eng gnet.Engine) gnet.Action {
 }
 
 func (es *EchoServer) OnTraffic(c gnet.Conn) gnet.Action {
+	player := c.Context().(*duel.DuelPlayer)
 	buf, _ := c.Next(-1)
-	c.Write(buf)
+	packetLen := int(binary.LittleEndian.Uint16(buf))
+	if packetLen > len(buf)-1 {
+		logging.Infof("conn[%v] refuse packet", c.RemoteAddr().String(), buf)
+		return gnet.Close
+	}
+	duel.HandleCTOSPacket(player, buf[2:])
+	return gnet.None
+}
+
+func (wss *EchoServer) OnOpen(c gnet.Conn) ([]byte, gnet.Action) {
+	player := &duel.DuelPlayer{
+		Conn: c,
+	}
+
+	c.SetContext(player)
+
+	return nil, gnet.None
+}
+
+func (wss *EchoServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+	if err != nil {
+		logging.Warnf("error occurred on connection=%s, %v\n", c.RemoteAddr().String(), err)
+	}
+	player := c.Context().(*duel.DuelPlayer)
+	duel.Leave(player)
+	//TODO 用户离开后离开房间
+	logging.Infof("conn[%v] disconnected", c.RemoteAddr().String())
 	return gnet.None
 }
